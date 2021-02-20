84b4112b7599fd8604cd66b9f4dfc1c51efb859d
Ical Risaldi <r.harisrisaldi@gmail.com>
2021-02-20 09:43:01 +0700

    sambatin

--- app/Http/Controllers/content_controller.php
+++ app/Http/Controllers/content_controller.php
@@ -33,7 +33,7 @@ class content_controller extends Controller
         ur.nama as namaResponden,
         u.nama as namaUser,
         (select count(*) from comment cm where cm.id_user = u.id) as jmlComment
-        from content c join user ur on c.id_responder = ur.id join user u on c.id_user = u.id  where c.id_responder in (select id_responder from folow f where f.id_user={$req->id_user}) or c.id_user = {$req->id_user}");
+        from content c join user ur on c.id_responder = ur.id join user u on c.id_user = u.id  where c.id_responder in (select id_responder from folow f where f.id_user={$req->id_user}) or c.id_user = {$req->id_user} order by c.create_at desc");
 
         return response()->json([
             'code' => 200,
@@ -44,20 +44,29 @@ class content_controller extends Controller
 
     public function insertContent(Request $req)
     {
-        $contentInsert = [
-            'gambar'=>$req->id_file,
-            'content'=>$req->content,
-            'id_user'=>$req->id_user,
-            'id_responder'=>$req->id_responder
-        ];
- 
-         $res = DB::table('content')
-         ->insert($contentInsert);
- 
-         return response()->json([
-            'code' => 200,
-            'message' => 'Sukses',
-            'data' => null
-        ]);
+        try {
+            $contentInsert = [
+                'signature'=>$req->signature,
+                'gambar'=>$req->gambar,
+                'content'=>$req->content,
+                'id_user'=>$req->id_user,
+                'id_responder'=>$req->id_responder
+            ];
+     
+             $res = DB::table('content')
+             ->insert($contentInsert);
+     
+             return response()->json([
+                'code' => 200,
+                'message' => 'Sukses',
+                'data' => null
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'code' => 500,
+                'message' => $th->getMessage(),
+                'data' => null
+            ]);
+        }
     }
 }
